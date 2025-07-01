9ca989aee828bed067ff9504f0058ee606e33789
Korey Ray <phentos@gmail.com>
2025-07-01 14:21:18 -0700

    modal example converted to crosspost, resolves #1

--- raistlin.py
+++ raistlin.py
@@ -13,14 +13,8 @@ GUILD = discord.Object(id=GUILD_ID)
 
 class MyClient(discord.Client):
     def __init__(self) -> None:
-        # Just default intents and a `discord.Client` instance
-        # We don't need a `commands.Bot` instance because we are not
-        # creating text-based commands.
         intents = discord.Intents.default()
         super().__init__(intents=intents)
-
-        # We need an `discord.app_commands.CommandTree` instance
-        # to register application commands (slash commands in this case)
         self.tree = app_commands.CommandTree(self)
 
     async def on_ready(self):
@@ -29,54 +23,56 @@ class MyClient(discord.Client):
         print('------')
 
     async def setup_hook(self) -> None:
-        # Sync the application command with Discord.
         await self.tree.sync(guild=GUILD)
 
 
-class Feedback(discord.ui.Modal, title='Feedback'):
-    # Our modal classes MUST subclass `discord.ui.Modal`,
-    # but the title can be whatever you want.
-
-    # This will be a short input, where the user can enter their name
-    # It will also have a placeholder, as denoted by the `placeholder` kwarg.
-    # By default, it is required and is a short-style input which is exactly
-    # what we want.
-    name = discord.ui.TextInput(
-        label='Name',
-        placeholder='Your name here...',
+class PostFetch(discord.ui.Modal, title='Post Fetch'):
+    postTitle = discord.ui.TextInput(
+        label='Title',
+        style=discord.TextStyle.short,
+        placeholder='Lost Mines of Phandelver',
+        required=True,
+        max_length=100
     )
 
-    # This is a longer, paragraph style input, where user can submit feedback
-    # Unlike the name, it is not required. If filled out, however, it will
-    # only accept a maximum of 300 characters, as denoted by the
-    # `max_length=300` kwarg.
-    feedback = discord.ui.TextInput(
-        label='What do you think of this new feature?',
-        style=discord.TextStyle.long,
-        placeholder='Type your feedback here...',
-        required=False,
-        max_length=300,
+    postLocation = discord.ui.TextInput(
+        label='Location',
+        style=discord.TextStyle.short,
+        default="Barley & Sword, North Park, San Diego",
+        required=True,
+        max_length=100
+    )
+
+    postHook = discord.ui.TextInput(
+        label='Hook',
+        style=discord.TextStyle.short,
+        placeholder="A snippet about the game/event to entice folks to join. Goes into the Event and Announcement.",
+        required=True,
+        max_length=200
+    )
+
+    postDescription = discord.ui.TextInput(
+        label='Description',
+        style=discord.TextStyle.paragraph,
+        placeholder="The full description of the game/event. Goes into the forum post.",
+        required=True
     )
 
     async def on_submit(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f'Thanks for your feedback, {self.name.value}!', ephemeral=True)
+        await interaction.response.send_message(f'Looking forward to {self.postTitle.value}!', ephemeral=False)
 
     async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
-        await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
+        await interaction.response.send_message('Oops! Something went wrong.', ephemeral=False)
 
-        # Make sure we know what the error actually is
         traceback.print_exception(type(error), error, error.__traceback__)
 
 
 client = MyClient()
 
 
-@client.tree.command(guild=GUILD, description="Submit feedback")
+@client.tree.command(guild=GUILD, description="Start a crosspost")
 async def conjure(interaction: discord.Interaction):
-    # Send the modal with an instance of our `Feedback` class
-    # Since modals require an interaction, they cannot be done as a response to a text command.
-    # They can only be done as a response to either an application command or a button press.
-    await interaction.response.send_modal(Feedback())
+    await interaction.response.send_modal(PostFetch())
 
 
 client.run(TOKEN)
